56d95ca37e7126279a49f0e8a436b7eefded473a
blewis <blewis@homer.(none)>
2013-03-08 15:49:39 -0500

    First working version of aggregate with the scidbdf class (but the syntax is slightly different than standard aggregate)

--- R/scidbdf-class.R
+++ R/scidbdf-class.R
@@ -53,23 +53,56 @@ setMethod('is.scidbdf', definition=function(x) return(FALSE))
 setGeneric('print', function(x) standardGeneric('print'))
 setMethod('print', signature(x='scidbdf'),
   function(x) {
-    cat(sprintf("A data frame-like reference to the 1D SciDB array %s (%.0f rows).\n",x@name, x@D$length))
+    cat(sprintf("SciDB array %s: %.0f obs. of %d variables.\n",x@name, x@D$length, length(x@attributes)))
   })
 
 setMethod('show', 'scidbdf',
   function(object) {
-    cat(sprintf("A data frame-like reference to the 1D SciDB array %s (%.0f rows).\n",object@name, object@D$length))
+    cat(sprintf("SciDB array %s: %.0f obs. of %d variables.\n",x@name, x@D$length, length(x@attributes)))
   })
 
-setGeneric('aggregate', function(formula,data,...) standardGeneric('aggregate'))
-setMethod("aggregate", signature("formula","scidbdf"),
-  function(formula,data,FUN,...)
+setOldClass("aggregate")
+setGeneric("aggregate", function(x,...) standardGeneric("aggregate"))
+setMethod("aggregate", signature(x="scidbdf"),
+  function(x,formula,FUN)
   {
+    data=x
+    if(missing(formula)) stop("Usage: aggregate(1D_scidb_array, formula, scidb_aggregate_expression)")
+    if(missing(FUN)) stop("Usage: aggregate(1D_scidb_array, formula, scidb_aggregate_expression)")
+# This is a hack until list('aggregates') returns type information, right now
+# we have to guess!!!
+    agtypes = list(approxdc="uint64 NULL",avg="double NULL",count="uint64 NULL",stdev="double NULL",var="double NULL")
     if(missing(FUN)) stop("You must supply a SciDB aggregate expression")
-    print(data@attributes)
-    print(formula)
     v = attr(terms(formula),"term.labels")
     r = setdiff(all.vars(formula),v)
-XXX
+    if(r==".") r = setdiff(data@attributes,v)
+# Redimension
+    A = tmpnam("array")
+
+    agat = strsplit(FUN,",")[[1]]
+    agnames = gsub(".* ","", gsub(" *$","",gsub("^ *","",gsub(".*)","",agat))))
+    if(any(nchar(agnames))<1) stop("We require that aggregate expressions name outputs, for example count(x) AS cx")
+    agfun = tolower(gsub(" *","",gsub("\\(.*","",agat)))
+    agtp = data@types[data@attributes %in% agnames]
+    agtp = paste(agtp, "NULL")
+    J = agfun %in% names(agtypes)
+    if(any(J)) agtp[J] = agtypes[agfun[J]]
+    attributes = paste(paste(agnames,agtp,sep=":"),collapse=",")
+
+    chunks = rep(1,length(v))
+    chunks[length(chunks)] = data@D$length
+    dims = paste(v,data@types[data@attributes %in% v],sep="(")
+    dims = paste(dims,"*",sep=")=")
+    dims = paste(dims,chunks,sep=",")
+    dims = paste(dims,",0",sep="")
+    dims = paste(dims,collapse=",")
+    schema = sprintf("<%s>[%s]",attributes,dims)
+    query = sprintf("create_array(%s,%s)",A, schema)
+    iquery(query)
+    on.exit(tryCatch(scidbremove(A),error=function(e)invisible()))
+    query = sprintf("redimension_store(%s,%s,%s)",data@name, A, paste(FUN,collapse=","))
+    iquery(query)
+    query = sprintf("scan(%s)",A)
+    iquery(query, `return`=TRUE)
   }
 )
