869006314b463df6330e6f09007b425965a33980
B. W. Lewis <blewis@illposed.net>
2014-03-22 00:37:17 -0400

    New glm_scidb and model_scidb functions

--- R/glm.R
+++ R/glm.R
@@ -28,7 +28,7 @@
 #
 
 # cf glm.fit
-glm.fit_scidb = function(x,y,`weights`=NULL,`family`=gaussian())
+glm.fit_scidb = function(x,y,weights=NULL,family=gaussian())
 {
   nobs = length(y)
   got_glm = length(grep("glm",.scidbenv$ops[,2]))>0
@@ -91,5 +91,213 @@ glm.fit_scidb = function(x,y,`weights`=NULL,`family`=gaussian())
     y = y,
     x = x
   )
+# BUG HERE IN SCIDB GLM AFFECTING binomial and poisson families?
+# THIS WILL BE FIXED IN 14.3 XXX XXX
+if(dist=="binomial" || dist=="poisson")
+{
+  ans$scidb_pval = ans$pval
+  ans$pval = 2*pnorm(-abs(ans$tval[]))
+}
   ans
+}
+
+
+
+# A really basic prototype glm function, limited to simple formulas and
+# treatment contrast encoding. cf glm.
+glm_scidb = function(formula, data, family=gaussian(), weights=NULL)
+{
+  if(!is.scidbdf(data)) stop("data must be a scidbdf object")
+  if(is.character(formula)) formula=as.formula(formula)
+  M = model_scidb(formula, data)
+  ans = glm.fit(M$model, M$response, weights=weights, family=family)
+  ans$formula = M$formula
+  ans$coefficient_names = M$names
+  ans$call = match.call()
+  class(ans) = "glm_scidb"
+  ans
+}
+
+.format = function(x)
+{
+  paste(capture.output(x),collapse="\n")
+}
+
+# cf print.glm
+print.glm_scidb = function(x, ...)
+{
+  ans = "Call:"
+  ans = paste(ans,.format(x$call),sep="\n")
+  ans = paste(ans,"Formula that was used:",sep="\n\n")
+  ans = paste(ans,.format(x$formula),sep="\n")
+  ans = paste(ans,.format(x$family),sep="\n")
+
+  cfs = coef(x)[]
+  names(cfs) = x$coefficient_names
+  ans = paste(ans,"Coefficients:",sep="\n")
+  ans = paste(ans,.format(cfs),sep="\n\n")
+  ans = paste(ans,sprintf("Null deviance: %.2f on %d degrees of freedom",x$null.deviance, x$df.null),sep="\n\n")
+  ans = paste(ans,sprintf("Residual deviance: %.2f on %d degrees of freedom",x$res.deviance, x$df.residual),sep="\n\n")
+  ans = paste(ans,sprintf("AIC: %.1f",x$aic),sep="\n\n")
+  cat(ans,"\n")
+}
+
+# cf summary.glm
+summary.glm_scidb = function(object, ...)
+{
+  x = object
+  ans = "Call:"
+  ans = paste(ans,.format(x$call),sep="\n")
+  ans = paste(ans,"Formula that was used:",sep="\n\n")
+  ans = paste(ans,.format(x$formula),sep="\n")
+  ans = paste(ans,.format(x$family),sep="\n\n")
+
+# Coefficient table
+  tbl_coef = coef(x)[]
+  tbl_stderr = x$stderr[]
+  tbl_zval = x$tval[]
+  p = x$pval[]
+  sig = c("***","**","*",".","")
+  star = sig[as.integer(cut(p,breaks=c(2e-16,0.001,0.01,0.05,0.1,1)))]
+  tbl = data.frame(tbl_coef, tbl_stderr, tbl_zval, p, star)
+  colnames(tbl) = c("Estimate", "Std. Error", "z value", "Pr(>|z|)","")
+  rownames(tbl) = x$coefficient_names
+  ans = paste(ans,.format(tbl),sep="\n\n")
+  ans = paste(ans,"---\nSignif. codes:  0 *** 0.001 ** 0.01 * 0.05 . 0.1   1",sep="\n")
+  ans = paste(ans,sprintf("Dispersion parameter: %.2f",x$dispersion),sep="\n\n")
+  ans = paste(ans,sprintf("Null deviance: %.2f on %d degrees of freedom",x$null.deviance, x$df.null),sep="\n\n")
+  ans = paste(ans,sprintf("Residual deviance: %.2f on %d degrees of freedom",x$res.deviance, x$df.residual),sep="\n\n")
+  ans = paste(ans,sprintf("AIC: %.1f",x$aic),sep="\n\n")
+  ans = paste(ans,sprintf("Number of Fisher Scoring iterations: %d",x$iter),sep="\n\n")
+  cat(ans,"\n")
+}
+
+# A limited version of a model matrix builder for linear models,
+# cf model.matrix and model.frame. Returns a model matrix for the scidbdf
+# object and the formula. String-valued variables in data are converted to
+# treatment contrasts, and if present a sparse model matrix is returned.
+# Returns a list of:
+# formula: The (possibly modified) formula associated with the model matrix
+# model:    A SciDB matrix with a single attribute named 'val' (model matrix)
+# response: A SciDB vector holding the response variable values
+# formual:  The formula
+# names:    The names of the model variables corresponding to the
+#           columns of 'model'
+model_scidb = function(formula, data)
+{
+  tryCatch(iquery("load_library('collate')"),
+    error=function(e) stop("This function requires the collate operator.\n See https://github.com/Paradigm4/collate"))
+  if(!is.scidbdf(data)) stop("data must be a scidbdf object")
+  if(is.character(formula)) formula=as.formula(formula)
+  dummy = data.frame(matrix(NA,ncol=length(scidb_attributes(data))))
+  names(dummy) = scidb_attributes(data)
+  t = terms(formula, data=dummy)
+  f = attr(t,"factors")
+  v = attr(t,"term.labels")
+  i = attr(t,"intercept")
+  r = attr(t,"response")
+
+  iname = c()
+  if(i==1)
+  {
+# Add an intercept term
+    iname = scidb:::make.unique_(data@attributes,"intercept")
+    data = bind(data, iname, "double(1)")
+  }
+
+  response = project(data,rownames(f)[r])
+  types = scidb:::scidb_types(data)
+  a = scidb_attributes(data)
+  factors = c()  # Will contain a list of factor variables
+  vars = c()     # Will contain a list of continuous variables
+# Check for unsupported formulae and warn.
+  ok = v %in% data@attributes
+  if(!all(ok))
+  {
+    not_supported = paste(v[!ok], collapse=",")
+    warning("Your formula is too complicated for this method. The following variables are not explicitly available and were not used in the model: ",not_supported)
+    formula = formula(drop.terms(t, which(v %in% not_supported), keep.response=TRUE))
+  }
+  w = which(data@attributes %in% c(v,iname))
+  if(length(w)<1) stop("No variables to model on")
+  for(j in w)
+  {
+    if(types[j]=="string")
+    {
+# Create a factor
+      factors = c(factors,unique(project(data,j)))
+      names(factors)[length(factors)] = a[j]
+      next
+    }
+    if(types[j]!="double")
+    {
+# Coerce to a double value
+      d = scidb:::make.unique_(a, sprintf("%s_double",a[j]))
+      expr = sprintf("double(%s)", a[j])
+      data = bind(data, d, expr)
+      vars = c(vars, d)
+      next
+    }
+    vars = c(vars, a[j])
+  }
+
+  varsstr = paste(vars, collapse=",")
+  query = sprintf("collate(project(%s,%s))",data@name,varsstr)
+  M = scidb:::.scidbeval(query,gc=TRUE,eval=TRUE)
+
+  if(length(factors)<1) return(list(model=M,response=response,formula=formula,names=vars))
+
+# Repartition to accomodate the factor contrasts, subtracting one
+# if an intercept term is present (i).
+  contrast_dim = 0
+  for(j in factors)
+  {
+    contrast_dim = contrast_dim + count(j) - i
+  }
+
+  newdim = ncol(M) + contrast_dim
+  newend = c(nrow(M)-1, newdim - 1)
+  newchunk = c(ceiling(1e6/newdim),newdim)
+
+  schema = sprintf("%s%s",scidb:::build_attr_schema(M),
+    scidb:::build_dim_schema(M,newstart=c(0,0),newend=newend,newchunk=newchunk))
+  M = reshape(M, shape=c(nrow(M), ncol(M)))  # Reset origin without moving data
+  M = redimension(M,schema=schema)           # Redimension to get extra columns
+
+# Merge in the contrasts
+  col = length(vars)
+  varnames = vars
+  for(j in 1:length(factors))
+  {
+    dn = scidb:::make.unique_(dimensions(data), "i")    # Naming conflicts are a big PITA
+    n  = names(factors)[j]
+    idx = sprintf("%s_index",n)
+    idx = scidb:::make.unique_(data@attributes, idx)
+    y = project(index_lookup(data, factors[[j]], n, idx), idx)
+    one = scidb:::make.unique_(y@attributes, "val")
+    column = scidb:::make.unique_(y@attributes, "j")
+    N = sprintf("%s%s",names(factors)[j],iquery(factors[[j]],return=TRUE)[,2])
+    if(i>0)
+    {
+# Intercept term present
+      y = subset(y, sprintf("%s > 0",idx))
+      y = bind(y, c(one,column), c("double(1)",sprintf("int64(%s + %d - 1)",idx,col)))
+      varnames = c(varnames, N[-1])
+      col = col + length(N) - 1
+    } else
+    {
+# No intercept term
+      y = bind(y, c(one,column), c("double(1)",sprintf("int64(%s + %d)",idx,col)))
+      varnames = c(varnames, N)
+      col = col + length(N)
+    }
+    schema = sprintf("%s%s",
+             scidb:::build_attr_schema(y,newnames=c("index","val","j")),
+             scidb:::build_dim_schema(y,newnames="i"))
+    y = redimension(reshape(cast(y,schema),shape=nrow(y)),M)
+# ... merge into M
+    M = merge(M,y,merge=TRUE) # eval this?
+  }
+
+  return(list(formula=formula,model=M,response=response,formula=formula,names=varnames))
 }
