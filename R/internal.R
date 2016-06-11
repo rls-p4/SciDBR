b24082bc0af3e20667a66584cbee97a6ba0cdfd9
Bryan W Lewis <blewis@paradigm4.com>
2016-06-11 01:12:30 -0400

    Issue in shim version ID from github hashes

--- R/internal.R
+++ R/internal.R
@@ -562,10 +562,10 @@ POST = function(data, args=list(), err=TRUE)
 # check for new shim simple post option (/upload), otherwise use
 # multipart/file upload (/upload_file)
   shimspl = strsplit(options("shim.version")[[1]], "\\.")[[1]]
-  shim_yr = as.integer(gsub("[A-z]", "", shimspl[1]))
-  shim_mo = as.integer(gsub("[A-z]", "", shimspl[2]))
-  if(is.na(shim_yr)) shim_yr = 14
-  if(is.na(shim_mo)) shim_mo = 1
+  shim_yr = tryCatch(as.integer(gsub("[A-z]", "", shimspl[1])), error=function(e) 16, warning=function(e) 8)
+  shim_mo = tryCatch(as.integer(gsub("[A-z]", "", shimspl[2])), error=function(e) 16, warning=function(e) 8)
+  if(is.na(shim_yr)) shim_yr = 16
+  if(is.na(shim_mo)) shim_mo = 8
   simple = (shim_yr >= 15 && shim_mo >= 7) || shim_yr >= 16
   if(simple)
   {
