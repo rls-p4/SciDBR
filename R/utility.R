fe66ce58c69802f20ef677ae884ecd62572a0125
Bryan W Lewis <blewis@paradigm4.com>
2016-09-22 06:39:59 -0400

    Fix for issue #114
    incorrect invocation of on.exit

--- R/utility.R
+++ R/utility.R
@@ -191,7 +191,7 @@ scidblist = function(pattern,
 {
   type = match.arg(type)
   lastprefix = getOption("scidb.prefix")
-  on.exit = options(scidb.prefix=lastprefix)
+  on.exit ({options(scidb.prefix=lastprefix)})
   options(scidb.prefix = paste(c(lastprefix, sprintf("set_namespace('%s')", namespace)), collapse=";"))
   if(n==Inf) n = -1   # non-intuitive read.table syntax
   Q = iquery(paste("list('",type,"')",sep=""), return=TRUE, nrows=n, binary=FALSE)
