1a11d38768e9547039dd3ce9a4714ecd7a5db40a
Bryan W Lewis <blewis@paradigm4.com>
2016-12-24 10:21:40 -0500

    More afl wrangling

--- R/afl.R
+++ R/afl.R
@@ -70,7 +70,7 @@ afl = function(...)
              lapply(as.list(match.call())[-1],
                function(.x) tryCatch({
                    ans = eval(.x)  # allow this to fail, handling error below
-                   if(inherits(ans, "scidb") || inherits(ans, "character")) return(ans)
+                   if(inherits(ans, "scidb") || inherits(ans, "character") || inherits(ans, "integer") || inherits(ans, "numeric") || inherits(ans, "logical")) return(ans)
                    stop()},
                  error=function(e) gsub("%as%", " as ", paste(capture.output(.x), collapse="")))),
              arg, attr(call, "conn"), .env), collapse=","))
