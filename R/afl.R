8069aff85480a017a3a63d4f4f0684513c5ba8de
Bryan W. Lewis <blewis@illposed.net>
2018-02-19 23:49:00 -0500

    Added back in the R(*) convention for 'quoting' R expressions that yield scalars

--- R/afl.R
+++ R/afl.R
@@ -1,20 +1,20 @@
 #' Update available AFL operators
 #'
-#' @param db an \code{afl} object (a SciDB database connection returned from \code{\link{scidbconnect}})
-#' @param new a character vector of operator names
-#' @param ops an optional three-variable data frame with variables name, signature, help, corresponding
+#' @param .db an \code{afl} object (a SciDB database connection returned from \code{\link{scidbconnect}})
+#' @param .new a character vector of operator names
+#' @param .ops an optional three-variable data frame with variables name, signature, help, corresponding
 #' to the operator names, signatures, and help files (from SciDB Doxygen documentation)
 #' @return an updated database \code{afl} object
 #' @note Every operator gets a shallow copy of the db argument; that is, \code{attributes(db[i])$conn} should be the same for every operator index i.
 #' @keywords internal
 #' @importFrom utils head
-update.afl = function(db, new, ops)
+update.afl = function(.db, .new, .ops)
 {
-  if (missing(ops))
+  if (missing(.ops))
   {
     e = new.env()
     data("operators", package="scidb", envir=e)
-    ops = e$operators
+    .ops = e$operators
   }
 
   afl = function(.db, .name, .help, .signature)
@@ -34,14 +34,14 @@ update.afl = function(db, new, ops)
                      assign(tail(make.names(c(ls(.depend), paste("V", runif(1), sep="")), unique=TRUE), 1), ex, envir=.depend)
                      ex@name
                    }
-                   else ex
+                   else .x
                }, error=function(e) .x)),
          collapse=",")
       expr = sprintf("%s(%s)", .name, .args)
       # handle aliasing
       expr = gsub("%as%", " as ", expr)
       # handle R scalar variable substitutions
-#      expr = rsub(expr, pf)
+      expr = rsub(expr, parent.frame())
       # Some special AFL non-operator expressions don't return arrays
       if (any(grepl(.name, getOption("scidb.ddl"), ignore.case=TRUE)))
       {
@@ -54,27 +54,27 @@ update.afl = function(db, new, ops)
     }
   }
 
-  for (.x in new)
+  for (.x in .new)
   {
-    formals = NULL
-    signature = ""
-    help = ""
-    .i = ops[, 1] == .x
+    .formals = NULL
+    .signature = ""
+    .help = ""
+    .i = .ops[, 1] == .x
     # update formal function arguments for nice tab completion help
     if (any(.i))
     {
-      def = head(ops[.i, ], 1)
+      .def = head(.ops[.i, ], 1)
       # XXX very ugly...
-      formals = strsplit(gsub("[=:].*", "", gsub("\\|.*", "", gsub(" *", "", gsub("\\]", "", gsub("\\[", "", gsub("\\[.*\\|.*\\]", "", gsub("[+*{})]", "", gsub(".*\\(", "", def[2])))))))), ",")[[1]]
-      help = def[3]
-      signature = def[2]
+      .formals = strsplit(gsub("[=:].*", "", gsub("\\|.*", "", gsub(" *", "", gsub("\\]", "", gsub("\\[", "", gsub("\\[.*\\|.*\\]", "", gsub("[+*{})]", "", gsub(".*\\(", "", .def[2])))))))), ",")[[1]]
+      .help = .def[3]
+      .signature = .def[2]
     }
-    db[[.x]] = afl(db, .x, help, signature)
-    if(!is.null(formals))
-      formals(db[[.x]]) = eval(parse(text=sprintf("alist(%s, ...=)", paste(paste(formals, "="), collapse=", "))))
-    class(db[[.x]]) = "operator"
+    .db[[.x]] = afl(db, .x, .help, .signature)
+    if(!is.null(.formals))
+      formals(.db[[.x]]) = eval(parse(text=sprintf("alist(%s, ...=)", paste(paste(.formals, "="), collapse=", "))))
+    class(.db[[.x]]) = "operator"
   }
-  db
+  .db
 }
 
 #' Substitute scalar-valued R expressions into an AFL expression
@@ -83,24 +83,24 @@ update.afl = function(db, new, ops)
 #' @param env environment in which to evaluate R expressions
 #' @return character valued AFL expression
 #' @keywords internal function
-#rsub = function(x, env)
-#{
-#  if (! grepl("[^[:alnum:]_]R\\(", x)) return(x)
-#  imbalance_paren = function(x) # efficiently find the first imbalanced ")" character position
-#  {
-#    which (cumsum( (as.numeric(charToRaw(x) == charToRaw("("))) - (as.numeric(charToRaw(x) == charToRaw(")"))) ) < 0)[1]
-#  }
-#  y = gsub("([^[:alnum:]_])R\\(", "\\1@R(", x)
-#  y = strsplit(y, "@R\\(")[[1]]
-#  expr = Map(function(x)
-#  {
-#    i = imbalance_paren(x)
-#    rexp = eval(parse(text=substring(x, 1, i - 1)), envir=env)
-#    rmdr = substring(x, i + 1)
-#    paste(rexp, rmdr, sep="")
-#  }, y[-1])
-#  sprintf("%s%s", y[1], paste(expr, collapse=""))
-#}
+rsub = function(x, env)
+{
+  if (! grepl("[^[:alnum:]_]R\\(", x)) return(x)
+  imbalance_paren = function(x) # efficiently find the first imbalanced ")" character position
+  {
+    which (cumsum( (as.numeric(charToRaw(x) == charToRaw("("))) - (as.numeric(charToRaw(x) == charToRaw(")"))) ) < 0)[1]
+  }
+  y = gsub("([^[:alnum:]_])R\\(", "\\1@R(", x)
+  y = strsplit(y, "@R\\(")[[1]]
+  expr = Map(function(x)
+  {
+    i = imbalance_paren(x)
+    rexp = eval(parse(text=substring(x, 1, i - 1)), envir=env)
+    rmdr = substring(x, i + 1)
+    paste(rexp, rmdr, sep="")
+  }, y[-1])
+  sprintf("%s%s", y[1], paste(expr, collapse=""))
+}
 
 #' Display SciDB AFL operator documentation
 #' @param topic an \code{\link{afl}} object from a SciDB database connection, or optionally a character string name
