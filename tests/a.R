035dc3436c6502dae7fab5d821044cf2063b05ad
Richard Samuels <rsamuels@paradigm4.com>
2023-01-16 11:50:40 -0500

    merge a.R changes from master

--- tests/a.R
+++ tests/a.R
@@ -12,7 +12,7 @@ library("Matrix")
 library("scidb")
 
 test_with_security = (Sys.getenv("SCIDB_TEST_WITH_SECURITY", "") == "true")
-is_arrow_installed = !is.null(tryCatch(library(arrow), error=function(e) NULL))
+is_arrow_installed = 'arrow' %in% .packages(all.available=TRUE)
 
 `%||%` = function(a, b) { if (length(a) > 0) a else b }
 
@@ -76,60 +76,73 @@ run_tests = function(db) {
   message("Starting tests on ", class(db)[[1]], " connection")
   t1 = proc.time()
 
-# 1 Data movement tests
+  message("--- 1. Data movement tests ---")
 
-# upload data frame
+  message("1.1. upload data frame")
   x = as.scidb(db, iris)
   a =  schema(x, "attributes")$name
-# binary download
+
+  message("1.2. binary download")
   check(iris[, 1:4], as.R(x)[, a][, 1:4])
-# iquery binary download
+
+  message("1.3. iquery binary download")
   check(iris[, 1:4], iquery(db, x, return=TRUE)[, a][, 1:4])
-# iquery CSV download
+
+  message("1.4. iquery CSV download")
   check(iris[, 1:4], iquery(db, x, return=TRUE, binary=FALSE)[, a][, 1:4])
-# iquery Arrow download
+
+  message("1.5. iquery Arrow download")
   if (is_arrow_installed) {
     check(iris[, 1:4], iquery(db, x, return=TRUE, 
                               arrow=TRUE, binary = FALSE)[, a][, 1:4])
   } else {
     warning("Arrow test was skipped because arrow library isn't installed")
   }
-# as.R only attributes
+
+  message("1.6. as.R only attributes")
   check(iris[, 1],  as.R(x, only_attributes=TRUE)[, 1])
 
-# only attributes and optional skipping of metadata query by supplying schema in full and abbreviated forms
+  message("1.7. only_attributes")
   check(as.R(db$op_count(x))$count, nrow(as.R(x)))
   check(as.R(db$op_count(x))$count, nrow(as.R(x, only_attributes=TRUE)))
+
+  message("1.8. supply full schema to skip metadata query")
   a = scidb(db, x@name, schema=schema(x))
   check(as.R(db$op_count(x))$count, nrow(as.R(a)))
+
+  message("1.9. supply abbreviated schema to skip metadata query")
   a = scidb(db, x@name, schema=gsub("\\[.*", "", schema(x)))
   check(as.R(db$op_count(x))$count, nrow(as.R(a)))
 
-# upload data frame using aio_input, download using only_attributes (because
-# aio_input adds dimensions we don't care about)
+  message("1.10. upload using aio_input")
+  ## download using only_attributes (because aio_input adds dimensions
+  ##  that we don't care about)
   x = as.scidb(db, iris, use_aio_input=TRUE)
   check(iris[, 1:4], as.R(x, only_attributes=TRUE)[, 1:4])
 
-# upload vector
+  message("1.11. upload vector")
   check(1:5, as.R(as.scidb(db, 1:5))[, 2])
-# upload matrix
+  
+  message("1.12. upload matrix")
   x = matrix(rnorm(100), 10)
   check(x, matrix(as.R(as.scidb(db, x))[, 3], 10, byrow=TRUE))
-# upload csparse matrix
-# also check shorthand projection syntax
+  
+  message("1.13. upload csparse matrix, also check shorthand projection syntax")
   x = Matrix::sparseMatrix(i=sample(10, 10), j=sample(10, 10), x=runif(10))
   y = as.R(as.scidb(db, x))
   check(x, Matrix::sparseMatrix(i=y$i + 1, j=y$j + 1, x=y$val))
-# issue #126
+  
+  message("1.14. issue #126")
   df = as.data.frame(matrix(runif(10*100), 10, 100))
   sdf = as.scidb(db, df)
   check(df, as.R(sdf, only_attributes=TRUE))
-# issue #130
+  
+  message("1.15. issue #130")
   df = data.frame(x1 = c("NA", NA), x2 = c(0.13, NA), x3 = c(TRUE, NA), stringsAsFactors=FALSE)
   x = as.scidb(db, df)
   check(df, as.R(x, only_attributes=TRUE))
 
-# upload n-d array
+  message("1.16. upload n-dimensional array")
   brick = 1:60
   dim(brick) = c(5, 4, 3)
   x = as.scidb(db, brick)
@@ -159,13 +172,15 @@ run_tests = function(db) {
   }
   check(brick_df, as.R(x))
 
-# garbage collection
+  ## Trigger garbage collection. Note this won't remove any arrays
+  ## because the local variables are still in scope - to really remove
+  ## the arrays, we need to gc() after this function exits.
   gc()
 
 
-# 2 AFL tests
+ message("\n--- 2. AFL tests ---")
 
-# Issue #128
+ message("2.1. Issue #128")
  i = 4
  j = 6
  x = db$build("<v:double>[i=1:2,2,0, j=1:3,1,0]", i * j)
@@ -175,40 +190,39 @@ run_tests = function(db) {
  check(as.integer(as.R(x)$w), rep(24, 6))
 
 
-# 3 Miscellaneous tests
+ message("\n--- 3. Miscellaneous tests ---")
 
-# issue #156 type checks
+ message("3.1. issue #156 type checks")
 
-# int64 option
+ message("3.1.1 reconnect with int64=TRUE")
  db = reconnect(db, int64=TRUE)
- 
  x = db$build("<v:int64>[i=1:2,2,0]", i)
  check(as.R(x), as.R(as.scidb(db, as.R(x, TRUE))))
 
+ message("3.1.2 reconnect with int64=FALSE")
  db = reconnect(db, int64=FALSE)
- 
  x = db$build("<v:int64>[i=1:2,2,0]", i)
  check(as.R(x), as.R(as.scidb(db, as.R(x, TRUE))))
 
-# Issue #157
+ message("3.2. Issue #157")
  x = as.R(scidb(db, "build(<v:float>[i=1:5], sin(i))"), binary = FALSE)
 
-# Issue #163
+ message("3.3. Issue #163")
  x = as.scidb(db, serialize(1:5, NULL))
  y = as.R(x)
  check(y$val[[1]], serialize(1:5,NULL))
 
  iquery(db, "build(<val:binary>[i=1:2,10,0], null)", return=TRUE)
 
-# Test for issue #161
+  message("3.4. Test for issue #161")
   iquery(db, "op_count(list())", return=TRUE, only_attributes=TRUE,  binary=FALSE)
 
-# Test for issue #158
+  message("3.5. Test for issue #158")
   x = iquery(db, "join(op_count(build(<val:int32>[i=0:234,100,0],random())),op_count(build(<val:int32>[i=0:1234,100,0],random())))", 
         schema = "<apples:uint64, oranges:uint64>[i=0:1,1,0]", return=TRUE)
   check(names(x), c("i", "apples", "oranges"))
 
-# issue #160 deal with partial schema string
+  message("3.6. issue #160 deal with partial schema string")
   x = iquery(db, "project(list(), name)", schema="<name:string>[No]", return=TRUE)
   check(names(x), c("No", "name"))
   iquery(db, "build(<val:double>[i=1:3;j=1:3], random())", return=T, schema="<val:double>[i; j]")
@@ -218,30 +232,32 @@ run_tests = function(db) {
   iquery(db, "build(<val:double>[i=1:3;j=1:3], random())", return=T, schema="<val:double>[i=1:3;j=1:3]")
   iquery(db, "build(<val:double>[i=1:3;j=1:3], random())", return=T, schema="<val:double>[i,j]")
 
-# basic types from scalars
+  message("3.7. basic types from scalars")
   lapply(list(TRUE, "x", 420L, pi), function(x) check(x, as.R(as.scidb(db, x))$val))
-# trickier types
+
+  message("3.8. trickier types")
   x = Sys.Date()
   check(as.POSIXct(x, tz="UTC"), as.R(as.scidb(db, x))$val)
   x = iris$Species
   check(as.character(x), as.R(as.scidb(db, x))$val)
-# type conversion from data frames
+
+  message("3.9. type conversion from data frames")
   x = data.frame(a=420L, b=pi, c=TRUE, d=factor("yellow"), e="SciDB", f=as.POSIXct(Sys.Date(), tz="UTC"), stringsAsFactors=FALSE)
 
-# issue #164 improper default value parsing
+  message("3.10. issue #164 improper default value parsing")
   tryCatch(iquery (db, "remove(x)"), error=invisible)
   iquery(db, "create array x <x:double not null default 1>[i=1:10]")
   as.R(scidb(db, "x"))
   tryCatch(iquery (db, "remove(x)"), error=invisible)
 
-# issue #158 support empty dimension spec []
+  message("3.11. issue #158 support empty dimension spec []")
   iquery(db, "apply(build(<val:double>[i=1:3], random()), x, 'abc')", return=TRUE,
          schema="<val:double,  x:string>[]", only_attributes=TRUE)
 
-# issue #172 (uint16 not supported)
+  message("3.12. issue #172 (uint16 not supported)")
   iquery(db, "list('instances')", return=TRUE, binary=TRUE)
 
-# Test for references and garbage collection in AFL statements
+  message("3.13. Test for references and garbage collection in AFL statements")
   x = store(db, db$build("<x:double>[i=1:1,1,0]", R(pi)))
   y = db$apply(x, "y", 2)
   rm(x)
@@ -249,7 +265,7 @@ run_tests = function(db) {
   as.R(y)
   rm(y)
 
-# Issue 191 scoping issue example
+  message("3.14. Issue 191 scoping issue example")
   a = db$build("<val:double>[x=1:10]", 'random()')
   b = db$aggregate(a, "sum(val)")
   as.R(b)
@@ -260,13 +276,13 @@ run_tests = function(db) {
   }
   foo()
 
-# Issue 193 Extreme numeric values get truncated on upload
+  message("3.15 Issue 193 Extreme numeric values get truncated on upload")
   upload_data <- data.frame(a = 1.23456e-50)
   upload_ref <- as.scidb(db, upload_data)
   download_data <- as.R(upload_ref, only_attributes = TRUE)
   stopifnot(upload_data$a == download_data$a)
 
-# Issue 195 Empty data.frame(s)
+  message("3.16 Issue 195 Empty data.frame(s)")
   for (scidb_type in names(scidb:::.scidbtypes)) {
     for (only_attributes in c(FALSE, TRUE)) {
       cat("Testing empty data frame of type ", scidb_type, 
@@ -293,7 +309,7 @@ run_tests = function(db) {
     }
   }
 
-# Issue 195 Coerce very small floating point values to 0
+  message("3.17 Issue 195 Coerce very small floating point values to 0")
   small_df <- data.frame(a = .Machine$double.xmin,
                          b = .Machine$double.xmin / 10,   # Will be coerced to 0
                          c = -.Machine$double.xmin,
@@ -306,7 +322,7 @@ run_tests = function(db) {
   print(small_df_db)
   check(small_df_db, small_df_fix)
   
-# Issue 217 Upload vectors, matrices as temp arrays via as.scidb
+  message("3.18. Issue 217 Upload vectors, matrices as temp arrays via as.scidb")
   iris_mod = iris
   colnames(iris_mod) = gsub(pattern = '[.]', replacement = '_', x = colnames(iris_mod))
   DF = as.scidb(db, iris_mod, temp = T)
@@ -324,7 +340,7 @@ run_tests = function(db) {
               iquery(db, "filter(list(), temporary=TRUE)", return = T)$name),
         TRUE)
   
-# Issue 220 Upload long vectors via as.scidb
+  message("3.19. Issue 220 Upload long vectors via as.scidb")
   if (test_with_security) { # The following tests for long vector upload do not run OK on Docker SciDB setups 
                             # that have low system RAM. 
                             # Disabling the tests for SciDB CE Docker setup (that is used on 
@@ -378,35 +394,42 @@ run_tests = function(db) {
     # Restoring global options
     options(scidb.max_byte_size = initial.max_byte_size)
     options(scidb.result_size_limit = initial.result_size_limit)
+  } else {
+    warning("Security test was skipped because SCIDB_TEST_WITH_SECURITY is not true")
   }
  
-# Issue 224 Support for SciDB dataframe
+  message("3.20. Issue 224 Support for SciDB dataframe")
   scidb_df_name = 'scidb_df_flat_test'
   scidb_df = data.frame(i=c(rep(1,3), rep(2,3), rep(3,3)), j=rep(1:3, 3), stringsAsFactors = FALSE)
   scidb_df$value = as.numeric(scidb_df$i == scidb_df$j)
-  # create a SciDB dataframe 
+  message("3.20.1. create a SciDB dataframe")
   iquery(db, 
          sprintf("store(flatten(build(<value:double>[i=1:3:0:1, j=1:3:0:1], iif(i=j, 1, 0))), %s)", 
                  scidb_df_name)
          )
-  # check iquery
+  message("3.20.2. check iquery")
   scidb_ret <- iquery(db, sprintf('scan(%s)', scidb_df_name), return = TRUE)
   scidb_ret <- scidb_ret[order(scidb_ret$i, scidb_ret$j),]
   check(scidb_df, scidb_ret)
-  # check as.R
+  message("3.20.3. check as.R")
   scidb_ret <- as.R(scidb(db, scidb_df_name))
   scidb_ret <- scidb_ret[order(scidb_ret$i, scidb_ret$j),]
   check(scidb_df, scidb_ret)
-  # Delete SciDB dataframe
+  message("3.20.4. Delete SciDB dataframe")
   iquery(db, sprintf('remove(%s)', scidb_df_name))
-  # Check an SciDB dataframe created from an array with a single dimension
+  message("3.20.5. Check an SciDB dataframe created from an array with a single dimension")
   scidb_ret <- iquery(db, 'flatten(build(<value:double>[i=1:5:0:1], iif(i%2=0, 1, 0)))', return=T)
   scidb_ret <- scidb_ret[order(scidb_ret$i),]
   check(scidb_ret, data.frame(i = 1:5, value = abs(floor(1:5%%2) -1), stringsAsFactors = FALSE))
-  
-  message("Ran tests in: ", (proc.time()-t1)[[3]], " seconds")
+
+  message("\n")
+  message("Ran tests on ", conn_type, " connection",
+          "in: ", (proc.time()-t1)[[3]], " seconds")
+  message("===== Finished tests on ", conn_type, " connection =====")
 }
 
+## Run tests on the given connection, then perform garbage collection
+## and check for arrays created by the test that weren't cleaned up.
 run_tests_with_gc = function(db) {
   preexisting <- ls(db)$name
   message("Pre-existing SciDB arrays: ", 
